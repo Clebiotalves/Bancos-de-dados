2e2d5439473eabce4641210660b92a9561ba9296
Clebio Teodoro Alves <106354414+Clebiotalves@users.noreply.github.com>
2023-01-05 20:39:28 -0300

    mudanças

---
+++ Python/contagemRegressiva.py
@@ -0,0 +1,39 @@
+'''from time import sleep
+for c in range(10,-1,-1):
+    print(c)
+    sleep(1)
+print('BUM! BUM! POWW!!!')'''
+
+'''for num in range(2,51,2):
+    print(num,end= ' ')
+print('Acabou')'''
+
+'''soma = 0
+cont = 0
+for c in range(1,501,2):
+    if c % 3 == 0:
+        cont += c
+        soma += 1
+print('A quantidade de numeros {} e tem {} de quantidade'.format(soma,cont))
+
+    
+    
+print('TABUADA')
+
+n = int(input('Digite um numero: '))
+for c in range(1,11):
+    print('{} x {} = {}'.format(n,c,n * c))
+
+
+
+
+soma = 0 
+cont = 0
+for n in range(0,6):
+    n = int(input('Digite um numero: '))
+    if n % 2 == 0:
+        soma += n
+        cont += 1
+print('Voce informou {} numeros pares e soma foi de {}'.format(cont,soma))  
+'''
+
---
+++ Python/loteria/loteria.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_excel("mega_sena_asloterias_ate_concurso_2551_sorteio.xlsx")
+print(df)
+
